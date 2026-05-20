bf661cdf8d463ef409aac781701420082666bd83
dzdev-26 <dzdevofficial@gmail.com>
2026-05-20 10:13:03 +0000

    commit

--- devcode-apk/android/app/src/main/java/com/dev/code/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.dev.code;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- devcode-apk/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.dev.code',
-  appName: 'DEV-CODE',
-  webDir: 'dist'
-};
-
-export default config;
